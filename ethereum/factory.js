a7ac6aa318f4f6b6dbb9d767586c9fdb9da9af38
subu-nair1798 <subunair17@gmail.com>
2020-07-28 10:35:48 +0530

    react and next.js setup

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from "./web3";
 import InsuranceFactory from "./build/InsuranceFactory.json"
 
-const contractAddress = ""
+const contractAddress = "0x40b843d2B5C71bD4769774384957f6E9437dFd6F"
 
 const instance = new web3.eth.Contract(
     InsuranceFactory.abi,
