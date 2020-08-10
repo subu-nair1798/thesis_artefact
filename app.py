f4f46fc1c87813ad07476a72acc5efbcaba2d7c4
subu-nair1798 <subunair17@gmail.com>
2020-08-10 21:22:52 +0530

    one hot encoding

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_cors import CORS, cross_origin
 from flask import Flask
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier
 # import pickle
 import pandas as pd 
@@ -29,24 +30,6 @@ def home():
     df["police_report_available"] = df["police_report_available"].replace(("YES", "NO"), (1, 0))
     df["property_damage"] = df["property_damage"].replace(("YES", "NO"), (1, 0))
 
-    le1 = LabelEncoder()
-    df["auto_make"] = le1.fit_transform(df["auto_make"])
-
-    le2 = LabelEncoder()
-    df["authorities_contacted"] = le2.fit_transform(df["authorities_contacted"])
-
-    le3 = LabelEncoder()
-    df["incident_severity"] = le3.fit_transform(df["incident_severity"])
-
-    le4 = LabelEncoder()
-    df["incident_type"] = le4.fit_transform(df["incident_type"])
-
-    le5 = LabelEncoder()
-    df["collision_type"] = le5.fit_transform(df["collision_type"])
-
-    le6 = LabelEncoder()
-    df["policy_csl"] = le6.fit_transform(df["policy_csl"])
-
     df1 = df.drop(["policy_number", "policy_bind_date", "policy_state", "insured_zip", "insured_education_level",
                    "insured_occupation", "insured_hobbies", "insured_relationship", "incident_date", "incident_state", 
                    "incident_city","incident_location", "auto_model", "_c39", "capital-gains", "capital-loss"], 
@@ -55,6 +38,12 @@ def home():
     x = df1.drop(["fraud_reported"], axis = 1)
     y = df1["fraud_reported"]
 
+    x = x.values
+    y = y.values
+
+    ct = ColumnTransformer(transformers = [("encoder", OneHotEncoder(), [2, 7, 8, 9, 10, 21])], remainder = "passthrough")
+    x = np.array(ct.fit_transform(x))
+
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 0)
 
     rf = RandomForestClassifier(n_estimators = 1000, random_state = 0)
@@ -62,18 +51,18 @@ def home():
 
     request_obj = request.args
     
-    predictedProbability = rf.predict_proba([[
+    predict_arg = ct.transform([[
         int(request_obj.get("monthsAsCustomer")),
         int(request_obj.get("age")),
-        le6.transform([pd.Series(request_obj.get("policyCsl"))])[0],
+        str(request_obj.get("policyCsl")),
         int(request_obj.get("policyDeductable")),
         float(request_obj.get("annualPremium")),
         int(request_obj.get("umbrellaLimit")),
         int(request_obj.get("sex")),
-        le4.transform([request_obj.get("incidentType")])[0],
-        le5.transform([request_obj.get("collisionType")])[0],
-        le3.transform([request_obj.get("incidentSeverity")])[0],
-        le2.transform([request_obj.get("authoritiesContacted")])[0],
+        str(request_obj.get("incidentType")),
+        str(request_obj.get("collisionType")),
+        str(request_obj.get("incidentSeverity")),
+        str(request_obj.get("authoritiesContacted")),
         int(request_obj.get("incidentHour")),
         int(request_obj.get("vehiclesInvolved")),
         int(request_obj.get("propertyDamage")),
@@ -84,9 +73,11 @@ def home():
         int(request_obj.get("injuryClaim")),
         int(request_obj.get("propertyClaim")),
         int(request_obj.get("vehicleClaim")),
-        le1.transform([request_obj.get("autoMake")])[0],
+        str(request_obj.get("autoMake")),
         int(request_obj.get("autoYear"))
     ]]) 
+
+    predictedProbability = rf.predict_proba(predict_arg)
 
     return str((predictedProbability[0][1]*100))
 
