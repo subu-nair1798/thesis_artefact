d992a217ea32fee4c3e2ac129caf473e5e05c89a
subu-nair1798 <36355359+subu-nair1798@users.noreply.github.com>
2020-09-25 11:45:56 +0100

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from "./web3";
 import InsuranceFactory from "./build/InsuranceFactory.json"
 
-const contractAddress = "0xa53ca17a1D47a747567e14d5023afbdCa4eBA78e"
+const contractAddress = "" // contract address of the rinkeby ethereum node
 
 const instance = new web3.eth.Contract(
     InsuranceFactory.abi,
